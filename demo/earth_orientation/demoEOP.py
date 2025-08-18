148d2a2d5b844bb86bfda7f234ee063e780b5cfc
Weihang Zhang <805545794@qq.com>
2025-08-18 19:12:54 +0800

    updates!

---
+++ demo/earth_orientation/demoEOP.py
@@ -0,0 +1,13 @@
+import numpy as np
+from pysrc.EOP.EarthOrientaition import EOP
+from pysrc.Auxiliary.FileTool import FileTool
+from pysrc.Auxiliary.MathTool import MathTool
+
+
+def demo1():
+    pass
+
+
+
+def demo2():
+    pass
