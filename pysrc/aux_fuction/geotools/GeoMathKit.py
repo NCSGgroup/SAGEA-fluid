357e55bd64fbd17f05279642012bd7db9ef91449
Weihang Zhang <805545794@qq.com>
2025-08-19 22:06:29 +0800

    leakage buffer method is added in auxiliary toolbox

--- pysrc/aux_fuction/geotools/GeoMathKit.py
+++ pysrc/aux_fuction/geotools/GeoMathKit.py
@@ -4,11 +4,46 @@ import gzip
 import math
 
 import numpy as np
+import scipy.ndimage as ndi
 
 class GeoMathKit:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def leakage(ocean_mask, lats, buffer_width_km):
+        """
+        parameters:
+        ocean_mask: 2D numpy (ocean=1, land=0)
+        lats: (1D)
+        lons: (1D)
+        buffer_width_km: km
+
+        return:
+        corrected_mask
+        """
+        if buffer_width_km <=0:
+            corrected_mask = ocean_mask
+        else:
+            # 1. compute grid resolution
+            dy = 111.32  # 1 degre of lat ≈ 111.32 km
+            dx_at_equator = 111.32  # 1 degree longitude in equator ≈ 111.32 km
+            # 2. mean grid resolution
+            mean_lat = np.mean(lats)
+            dx = dx_at_equator * np.cos(np.deg2rad(mean_lat))
+            mean_resolution_km = np.sqrt(dx ** 2 + dy ** 2) / 2  # approximately mean grid resolution
+            # 3. the points of buffer
+            buffer_cells = int(np.ceil(buffer_width_km / mean_resolution_km))
+            # 4. find the boundary
+            land = (ocean_mask == 0).astype(int)
+            structure = ndi.generate_binary_structure(2, 2)  # 8连通结构(3x3)
+            dilated_land = ndi.binary_dilation(land, structure=structure, iterations=buffer_cells)
+            # 5. correct mask
+            buffer_zone = dilated_land & (ocean_mask == 1)
+            corrected_mask = ocean_mask.copy()
+            corrected_mask[buffer_zone] = 0
+        return corrected_mask
 
     @staticmethod
     def generate_month_range(start_str, end_str):
