eaa8b9722cacf306f42b9ddab709ec40e4ce8a05
Weihang Zhang <805545794@qq.com>
2026-01-13 18:58:53 +0800

    Some examples of how to use this code to conduct OLS have added.

--- pysrc/ancillary/geotools/GeoMathKit.py
+++ pysrc/ancillary/geotools/GeoMathKit.py
@@ -45,6 +45,44 @@ class GeoMathKit:
             corrected_mask[buffer_zone] = 0
         return corrected_mask
 
+
+    @staticmethod
+    def leakage_land(land_mask, lats, buffer_width_km):
+        """
+        parameters:
+        ocean_mask: 2D numpy (ocean=1, land=0)
+        lats: (1D)
+        lons: (1D)
+        buffer_width_km: km
+
+        return:
+        final_mask
+        """
+        ocean_mask = 1-land_mask
+        if buffer_width_km <=0:
+            corrected_mask = ocean_mask
+        else:
+            # 1. compute grid resolution
+            dy = 111.32  # 1 degre of lat ≈ 111.32 km
+            dx_at_equator = 111.32  # 1 degree longitude in equator ≈ 111.32 km
+            # 2. mean grid resolution
+            mean_lat = np.mean(lats)
+            dx = dx_at_equator * np.cos(np.deg2rad(mean_lat))
+            mean_resolution_km = np.sqrt(dx ** 2 + dy ** 2) / 2  # approximately mean grid resolution
+            # 3. the points of buffer
+            buffer_cells = int(np.ceil(buffer_width_km / mean_resolution_km))
+            # 4. find the boundary
+            land = (ocean_mask == 0).astype(int)
+            structure = ndi.generate_binary_structure(2, 2)  # 8连通结构(3x3)
+            dilated_land = ndi.binary_dilation(land, structure=structure, iterations=buffer_cells)
+            # 5. correct mask
+            buffer_zone = dilated_land & (ocean_mask == 1)
+            corrected_mask = ocean_mask.copy()
+            corrected_mask[buffer_zone] = 0
+
+        final_mask = 1-corrected_mask
+        return final_mask
+
     @staticmethod
     def generate_month_range(start_str, end_str):
         """
@@ -85,5 +123,31 @@ class GeoMathKit:
         else:
             coeffs = np.einsum('ij,jk->ik',inv_matrix,y,optimize=True)
         return coeffs
+
+def demo1():
+    y_value = np.array([1,2,3,4,5,6,7,8,9,10])
+    t_axis = np.linspace(2002,2003,len(y_value))
+
+    coefs = GeoMathKit.linear_fit(t=t_axis, y=y_value[:, None], Dimenson=1)
+
+    amp = np.sqrt(coefs[2] ** 2 + coefs[3] ** 2)
+
+    print(f"trend:{coefs[1]},amplitude:{amp}")
+
+
+def demo2():
+    y_value = np.random.uniform(0,100,size=(100,180,360))
+    t_axis = np.linspace(2002, 2003, len(y_value))
+
+    coefs = GeoMathKit.linear_fit(t=t_axis, y=y_value, Dimenson=2)
+
+    amp = np.sqrt(coefs[2] ** 2 + coefs[3] ** 2)
+
+    print(f"trend:{coefs[1].shape},amplitude:{amp.shape}")
+
+if __name__ == "__main__":
+    demo2()
+
+
 
 
